049bea52303c56e94d92c54a16591bdd803f9b53
Aditya Patil <162578215+Aditya-8888@users.noreply.github.com>
2026-06-24 20:05:27 +0530

    Create Day-02-Aggregate-Functions.sql
    Practiced SQL aggregate functions including COUNT, SUM, AVG, MIN, and MAX. Added sample queries for interview preparation and data analytics learning.

---
+++ Day-02-Aggregate-Functions.sql
@@ -0,0 +1,17 @@
+Aggregate Functions (COUNT, SUM, AVG, MIN, MAX)
+
+
+-- COUNT
+SELECT COUNT(*) FROM Employees;
+
+-- SUM
+SELECT SUM(Salary) FROM Employees;
+
+-- AVG
+SELECT AVG(Salary) FROM Employees;
+
+-- MIN
+SELECT MIN(Salary) FROM Employees;
+
+-- MAX
+SELECT MAX(Salary) FROM Employees;
